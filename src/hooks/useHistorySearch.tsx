a07355158027b488392fe07b202dec32a32c8868
toono <52028799+toono-f@users.noreply.github.com>
2023-12-16 12:54:58 +0900

    Refactor history search queries

--- src/hooks/useHistorySearch.tsx
+++ src/hooks/useHistorySearch.tsx
@@ -8,25 +8,6 @@ const whereClauses = (tableTitle: string, terms: string[], tableUrl?: string) =>
   return terms.map((t) => `${tableTitle}.title LIKE '%${t}%'`).join(" AND ");
 };
 
-const getWebKitHistoryQuery = (table: string, date_field: string, terms: string[]) =>
-  `SELECT history_items.id as id, url, history_visits.title as title, datetime(${date_field}+978307200, "unixepoch", "localtime") as lastVisited
-  FROM ${table}
-    INNER JOIN history_visits
-    ON history_visits.history_item = history_items.id
-  WHERE ${whereClauses("history_visits", terms, "history_items")}
-  GROUP BY url
-  ORDER BY ${date_field} DESC
-  LIMIT 30
-  `;
-
-const getOrionHistoryQuery = (table: string, date_field: string, terms: string[]) =>
-  `SELECT ID as id, URL as url, TITLE as title, datetime(${date_field}+978307200, "unixepoch", "localtime") as lastVisited
-  FROM ${table}
-  WHERE ${whereClauses(table, terms)}
-  ORDER BY ${date_field} DESC
-  LIMIT 30
-  `;
-
 const getChromiumGeckoHistoryQuery = (table: string, date_field: string, terms: string[]) => `SELECT
     id, url, title,
     datetime(${date_field} / 1000000 + (strftime('%s', '1601-01-01')), 'unixepoch', 'localtime') as lastVisited
